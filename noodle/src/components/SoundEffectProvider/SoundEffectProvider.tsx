b42ab5506adff248d9bb414f96c1325a89cb5828
TrevorShelby <rovertdude@gmail.com>
2024-10-29 20:43:39 -0500

    Update SoundEffectProvider.tsx

--- noodle/src/components/SoundEffectProvider/SoundEffectProvider.tsx
+++ noodle/src/components/SoundEffectProvider/SoundEffectProvider.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Howl } from 'howler';
+import { Howl, Howler } from 'howler';
 import soundsMp3 from '../../sounds/sounds.mp3';
 import soundsWebm from '../../sounds/sounds.webm';
 import { useMediaReadiness } from '@src/media/useMediaReadiness';
@@ -8,31 +8,37 @@ export type SoundEffectName = 'join';
 
 export const SoundEffectContext = React.createContext<{ playSound: (name: SoundEffectName) => void } | null>(null);
 
-// global Howl object to play sounds with
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const howl = new Howl({
   src: [soundsMp3, soundsWebm],
-  // sounds are arranged into a single file, and we use timeslices to name specific sounds within the file
   sprite: {
     join: [0, 5000],
   },
 });
 
 export const SoundEffectProvider: React.FC = ({ children }) => {
-  // we'll track the media readiness state and only begin playing sounds when it's ready.
   const isReady = useMediaReadiness((s) => s.isReady);
-  // to keep the `play` callback reference stable we copy it to a ref
   const isReadyRef = React.useRef(isReady);
   React.useLayoutEffect(() => {
     isReadyRef.current = isReady;
   }, [isReady]);
 
+  // Add a click event listener to resume the audio context
+  React.useEffect(() => {
+    const handleUserGesture = () => {
+      if (Howler.ctx && Howler.ctx.state === 'suspended') {
+        Howler.ctx.resume();
+      }
+    };
+
+    document.addEventListener('click', handleUserGesture);
+    return () => document.removeEventListener('click', handleUserGesture);
+  }, []);
+
   const play = React.useCallback((name: SoundEffectName) => {
     if (!isReadyRef.current) return;
-    // TODO: Renable this once we are done with our demo
-    // 1-28-2021
-    //howl.play(name);
+    howl.play(name);
   }, []);
 
   return <SoundEffectContext.Provider value={{ playSound: play }}>{children}</SoundEffectContext.Provider>;
 };
+
